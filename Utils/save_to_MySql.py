e070e3f5d1ce1d137f67b23e85c9d1d8728ad65a
Rwqkobe <rwqkobe@gmail.com>
2018-04-07 17:55:20 +0800

    增加了vehicle

---
+++ Utils/save_to_MySql.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+DB_CONNECT_STRING = 'mysql+mysqldb://root:123456@localhost/'
+
+
+class MySqlSaver(object):
+    def __init__(self, db_name):
+        engine = create_engine(DB_CONNECT_STRING + db_name, echo=True)
+        Base.metadata.create_all(engine)
+        DBSession = sessionmaker(bind=engine)
+        self.session = DBSession()
+
+    def save(self, data_object):
+        try:
+            self.session.add(data_object)
+            self.session.commit()
+        except:
+            self.session.rollback()
+            raise
+        finally:
+            self.session.close()
--- config.py
+++ config.py
@@ -1,3 +1,9 @@
-data_dict={'Pedestrian':'person',
-           'Vehicle':'vehicle',
-           'DMS':'head',}
+data_dict = {'Pedestrian': 'person',
+             'Vehicle': 'vehicle',
+             'DMS': 'head', }
+
+data_path_dict = {'Pedestrian_test': r'D:\项目汇总\ADAS\行人\TestingSet\ADAS行人-测试集JSON',
+             'Pedestrian_train': None,
+             'DMS_test': r'D:\数据分布\DMS\dms_test_data',
+             'Vehicle_test': r'D:\项目汇总\ADAS\车头车尾\TestingSet\ADAS车辆-测试集JSON',
+             'Vehicle_train': r'D:\数据分布\车辆\train_data'}
